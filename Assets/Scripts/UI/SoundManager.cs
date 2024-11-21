c54941ee25df760c43bb7b32a65a0d4e3464a09b
1PKure <pulidomatias@gmail.com>
2024-11-21 15:26:49 -0300

    WebGL Builds

--- Assets/Scripts/UI/SoundManager.cs
+++ Assets/Scripts/UI/SoundManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
 using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
